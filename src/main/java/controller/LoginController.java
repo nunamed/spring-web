20974318faae6e8d0f6fce87639c5cd471a6b97a
nunamed <1736633080@qq.com>
2023-03-10 02:42:08 +0800

    springmvc

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoginController implements Controller{
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return new ModelAndView("register.jsp");
+    }
+    
+}
---
+++ src/main/java/controller/RegisterController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class RegisterController implements Controller{
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest arg0,HttpServletResponse arg1)throws Exception{
+        return new ModelAndView("register.jsp");
+    }
+}
